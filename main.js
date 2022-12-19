755f0fede2f391466be0ccd8ff1c8b34129fea02
Jonathan Dressel <Jonathan.dressel@retireready.com>
2022-12-19 14:38:27 -0800

    Adding all *.js and *.html files

---
+++ main.js
@@ -0,0 +1,67 @@
+  $("path, circle").hover(function(e) {
+    $('#info-box').css('display','block');
+    $('#info-box').html($(this).data('info'));
+  });
+  
+  jQuery(document).ready(function() {
+    $("#stateE").text("Oregon");
+    $("#crimeE").text("40");
+    $("#dangerE").text("LOW");
+  });
+
+  $("#g5").click(function() {
+    $('table tr td').each(function() {
+      var customerId = $(this).find(".stateE");    
+      console.log("ENT: " + customerId)
+    });
+
+    var state = $(".state").text().replace(/State: /g,'');
+    var crimeE = $(".rate").text().replace(/Crime Rate: /g,'');
+    console.log("RATE : " + crimeE);
+    // var crimeE = Math.floor(Math.random() * 100);
+
+    if(crimeE > 75) {
+      addRow(state, crimeE, "HIGH");
+      $("path:hover, circle:hover").css('fill', '#ff0011');
+
+    }
+    else if(crimeE < 75 && crimeE > 50) {
+      addRow(state, crimeE, "MEDIUM");
+      $("path:hover, circle:hover").css('fill', '#ffcc00');
+    }
+    else if(crimeE < 50 && crimeE > 25) {
+      addRow(state, crimeE, "LOW");
+      $("path:hover, circle:hover").css('fill', '#99cc33');
+    }
+    else {
+      addRow(state, crimeE, "VERY SAFE");
+      $("path:hover, circle:hover").css('fill', '#339900');
+    }
+  })
+
+  $("path, circle").mouseleave(function(e) {
+    $('#info-box').css('display','none');
+  });
+  
+  $(document).mousemove(function(e) {
+    $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
+    $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
+  }).mouseover();
+  
+  function addRow(state, crime, danger) {
+    $("table tr:last").after("<tr><td id='stateE'>" + state + "</td><td id='crimeE'>" + crime + 
+      "</td><td id='dangerE'>" + danger + "</td></tr>");
+  }
+
+  $.getJSON('https://api.usa.gov/crime/fbi/sapi/api/participation/dl/national?API_KEY=HtrUjiet5nq07XWQ0hY5f6YegK6Iab0WZ76phfK0').success(function(data) {
+    console.log(JSON.parse(data));
+  })
+
+  var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  if(ios) {
+    $('a').on('click touchend', function() {
+      var link = $(this).attr('href');
+      window.open(link,'_blank');
+      return false;
+    });
+  }
